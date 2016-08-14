c0d23d5f8cc80e1f6f586342e8ff5af464c547ac
Shinya Kitamura <shinya.kitamura@sugar.sg>
2016-08-15 02:48:17 +0800

    Add week_view ViewObject into Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_week, only: [:index]
 
   def index
     @events = Event.all
@@ -46,5 +47,9 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:name, :start, :end, :order)
+  end
+
+  def set_week
+    @week = WeekView.new(params[:date])
   end
 end
---
+++ app/view_objects/week_view.rb
@@ -0,0 +1,36 @@
+class WeekView
+  def initialize(date)
+    date = date.present? ? Date.parse(date) : Date.today
+    @start_date = date - date.cwday
+  end
+
+  def sunday
+    @start_date.strftime("%A %m/%d")
+  end
+
+  def monday
+    (@start_date+1).strftime("%A %m/%d")
+  end
+
+  def tuesday
+    (@start_date+2).strftime("%A %m/%d")
+  end
+
+  def wednesday
+    (@start_date+3).strftime("%A %m/%d")
+  end
+
+  def thursday
+    (@start_date+4).strftime("%A %m/%d")
+  end
+
+  def friday
+    (@start_date+5).strftime("%A %m/%d")
+  end
+
+  def saturday
+    (@start_date+6).strftime("%A %m/%d")
+  end
+
+
+end
