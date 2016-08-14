5b79976c93897e9307084e4713201d79a1162f45
Shinya Kitamura <shinya.kitamura@sugar.sg>
2016-08-15 01:49:32 +0800

    Add Event scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  resources :events
+  root to: 'events#index'
 end
---
+++ spec/factories/events.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :event do
+    name "MyString"
+    start_time "2016-08-15 01:40:09"
+    end_time "2016-08-15 01:40:09"
+    order 1
+  end
+end
